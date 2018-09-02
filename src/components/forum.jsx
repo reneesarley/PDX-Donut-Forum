aba4bc2b0d6ac8e778a357c2671a6f32509b05bc
Renee Sarley <reneesarley@gmail.com>
2018-09-02 13:01:34 -0700

    convert forum to stateful component

--- src/components/forum.jsx
+++ src/components/forum.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import Post from './Post';
 
-const posts = [
-  {
-    postName: 'My First Post',
-    postContent: 'This is my first post to this forum'
-  },
-  {
-    postName: 'I\'m posting again!',
-    postContent: 'Here is my second post. I am getting really good at this.'
+class Forum extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      masterPostList: [
+        {
+          postName: 'My First Post',
+          postContent: 'This is my first post to this forum'
+        },
+        {
+          postName: 'I\'m posting again!',
+          postContent: 'Here is my second post. I am getting really good at this.'
+        }
+      ]
+    };
   }
-];
-
-function Forum(){
-  return (
-    <div>
-      <h1>Portland Donut Forum</h1>
-      <h3>This is the place to talk about the PDX donut scene!</h3>
-      {posts.map((post, index) =>
-        <Post
-          postName={post.postName}
-          postContent={post.postContent}
-          key={index}
-        />
-      )}
-
-    </ div>
-  );
+  render(){
+    return (
+      <div>
+        <h1>Portland Donut Forum</h1>
+        <h3>This is the place to talk about the PDX donut scene!</h3>
+        {this.state.masterPostList.map((post, index) =>
+          <Post
+            postName={post.postName}
+            postContent={post.postContent}
+            key={index}
+          />
+        )}
+      </ div>
+    );
+  }
 }
 
 export default Forum;
